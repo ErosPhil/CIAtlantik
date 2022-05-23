ba2367bc88fbc4ab1b31afc4816d1eee5fe8b445
eros bts <eros.bts@outlook.com>
2022-05-23 23:48:36 +0200

    23/05

--- application/views/visiteur/accueil.php
+++ application/views/visiteur/accueil.php
@@ -1,4 +1,38 @@
 <html>
-    <p> Ceci est la page d'accueil </p>
-    <P> <?php echo img('Atlantik.jpg')?>
+    <div class="container">  
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicateurs -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="Atlantik.jpg" alt="Image1" style="width:100%;">
+                </div>
+
+                <div class="item">
+                    <img src="chicago.jpg" alt="Image2" style="width:100%;">
+                </div>
+                
+                <div class="item">
+                    <img src="ny.jpg" alt="Image3" style="width:100%;">
+                </div>
+            </div>
+
+            <!-- Boutons gauche et droite -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Précédent</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Suivant</span>
+            </a>
+        </div>
+    </div>
+    <?php echo img('Atlantik.jpg')?>
 </html>
--- application/views/visiteur/liste_secteurs.php
+++ application/views/visiteur/liste_secteurs.php
@@ -1,9 +1,11 @@
-<div position: absolute;>
-    <ul>
-        <?php
-            foreach($lesSecteurs as $unSecteur):
-                echo '<li>'?> <a href="<?php echo site_url('visiteur/afficherHorairesTraversees/'.$unSecteur->nosecteur) ?>"> <?php echo $unSecteur->nomsecteur.'</a></li>' ;
-            endforeach;
-        ?>
-    </ul>
+<div class="container bg-dark">
+    <div class="row">
+        <div class="col-sm-2">
+            <?php
+                foreach($lesSecteurs as $unSecteur):
+                    ?> <a href="<?php echo site_url('visiteur/afficherHorairesTraversees/'.$unSecteur->nosecteur) ?>"> <?php echo $unSecteur->nomsecteur.'</a><br>' ;
+                endforeach;
+            ?>
+        </div>
+    </div>
 </div>
